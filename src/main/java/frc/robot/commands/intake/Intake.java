7b6981a8293ca2f9859e2f93e40933fa03764f6c
zhoutony05 <63679813+zhoutony05@users.noreply.github.com>
2022-11-18 15:09:16 -0800

    Intake Subsystem

---
+++ src/main/java/frc/robot/commands/intake/Intake.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.Intake;
+
+public class Intake extends CommandBase{
+
+  Intake m_intake;
+  
+  public Intake(){
+    this(Robot.intake);
+  }
+
+  public Intake(Intake subsystem){
+    m_intake = subsystem;
+    addRequirements(m_singulator);
+  }
+  
+  @Override
+  public void execute(){
+    m_intake.set(0.3);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_intake.stop();
+  }
+
+}
